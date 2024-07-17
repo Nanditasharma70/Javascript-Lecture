5c065f5a324bf1fa5235175009ac4ca0b817ca11
Nandita <nanditasharma9910@gmail.com>
2024-07-17 15:14:52 +0530

    operation

--- o3.conversionOperation.js
+++ o3.conversionOperation.js
@@ -1,21 +1,55 @@
-let score ="33"
-console.log(typeof score);
-console.log(typeof(score));
+// let score ="33"
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-//"33"  => 33
-// 33abc=> NaN
+// //"33"  => 33
+// // 33abc=> NaN
 
-//True=> 1, false=> 0
+// //True=> 1, false=> 0
 
-let isloggedIn = "Nandita"
-let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn)
+// let isloggedIn = "Nandita"
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
 
-let someNumber = 35
+// let someNumber = 35
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//***************************Operations********************************//
+
+let value= 3
+let ngvalue= -value
+// console.log(ngvalue)
+
+console.log(2+2)
+console.log(2*2)
+console.log(4-2)
+console.log(2/2)
+console.log(4/2)
+console.log(4%2)
+console.log(4%3)
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(3 + 4 * 5 % 3);
+console.log(3 + 4 * (5 % 3));
+ console.log(+true)
+ console.log(+"")
+
+ let num1, num2, num3
+
+ num1 = num2 =num3 = 2+2
+ let gameCouter= 1000
+  gameCouter ++;
+  ++gameCouter;
