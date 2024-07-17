30a8f0d78650c8d8045c964fad8d250f636169d2
Nandita <nanditasharma9910@gmail.com>
2024-07-17 14:36:18 +0530

    datatypes complete

---
+++ o2.datatypes.js
@@ -0,0 +1,45 @@
+"use strict"; //treat all js code as a  newer version
+
+// alert(3+3) // we are using node js not browser...
+
+console.log(3+3)
+ console.log("Nandita") // code readibilit should be high
+
+ let name= "Nandita"
+ let age = 21
+ let isMarried = false
+ //  let isMarried = true // boolean value
+ //  let isMarried = 1 // boolean value
+ //  let isMarried = "true" // boolean value
+ //  let isMarried = "false" // boolean value
+ //  let isMarried = "yes" // boolean value
+ //  let isMarried = "no" // boolean value
+
+ //number
+ let num1 = 10
+ let num2 = 20
+ let num3 = 30
+ //string
+ let str1 = "Nandita"
+ let str2 = "Nandita"
+ //boolean
+ let bool1 = true
+ let bool2 = false
+    
+ //null
+ let null1 = null
+ //undefined
+ let undefined1 = undefined // value not assign in a variables
+ //object
+ let obj1 = {name:"Nandita",age:21}
+ //array
+ let arr1 = [1,2,3,4,5,6,7,8
+    ]
+    //function
+    let func1 = function(){} //function declaration
+    let func2 = () => {} //function expression
+    //symbol
+    let sym1 = Symbol("Nandita") //unique value
+    //bigInt
+    let bigInt1 = BigInt(10) //number with big value
+    
