909566be857757d960cf0e8c1c9d801a2d347bf8
Nandita <nanditasharma9910@gmail.com>
2024-07-17 14:50:02 +0530

    conversion operation

---
+++ o3.conversionOperation.js
@@ -0,0 +1,21 @@
+let score ="33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"  => 33
+// 33abc=> NaN
+
+//True=> 1, false=> 0
+
+let isloggedIn = "Nandita"
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber = 35
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
